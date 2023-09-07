9d6afc6439bd0512fd9aeb69526a7f67b38b581f
Jakub Kulik <jakub.kulik1@o2.pl>
2023-09-07 10:11:36 +0200

    js functions

--- JS/script.js
+++ JS/script.js
@@ -2,26 +2,32 @@ let button = document.querySelector(".js-button");
 
 let body = document.querySelector(".js-body");
 let table = document.querySelector(".js-table");
+const welcome = () => {
+    console.log("Hello Developer");
+}
+const onChangeBackgroundClick = () => {
+    button.addEventListener("click", () => {
+        button.classList.toggle("darkMode");
+        table.classList.toggle("darkMode");
+        body.classList.toggle("darkMode");
+    })
+    button.addEventListener("click", () => {
+        button.classList.toggle("lightMode");
+        table.classList.toggle("lightMode");
+        body.classList.toggle("lightMode");
+    })
 
-button.addEventListener("click", () => {
-    button.classList.toggle("darkMode");
-    table.classList.toggle("darkMode");
-    body.classList.toggle("darkMode");
-})
-button.addEventListener("click", () => {
-    button.classList.toggle("lightMode");
-    table.classList.toggle("lightMode");
-    body.classList.toggle("lightMode");
-})
+    button.addEventListener("click", () => {
+        if (button.classList.contains("lightMode")) {
+            button.innerText = "Zmień na ciemny motyw";
 
-button.addEventListener("click", () => {
-    if (button.classList.contains("lightMode")) {
-        button.innerText = "Zmień na ciemny motyw";
+        }
+        else if (button.classList.contains("darkMode")) {
+            button.innerText = "Zmień na jasny motyw";
+        }
+    })
+};
 
-    }
-    else if (button.classList.contains("darkMode")) {
-        button.innerText = "Zmień na jasny motyw";
-    }
-})
-
+welcome();
+onChangeBackgroundClick();
 
